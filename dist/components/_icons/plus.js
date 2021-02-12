6d8a4d6498225aa7de1ae8881d30b55c762730a4
michaeladze <michael.kutateladze@yandex.ru>
2021-02-12 23:31:09 +0300

    feat: [master] Modals and some css

--- dist/components/_icons/plus.js
+++ dist/components/_icons/plus.js
@@ -15,5 +15,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = __importDefault(require("react"));
-exports.default = (function (props) { return (react_1.default.createElement("svg", __assign({ width: '24', height: '24', viewBox: '0 0 24 24', fill: 'none', xmlns: 'http://www.w3.org/2000/svg' }, props),
-    react_1.default.createElement("path", { d: 'M19 11H13V5H11V11H5V13H11V19H13V13H19V11Z', fill: 'currentColor' }))); });
+exports.default = (function (props) { return (react_1.default.createElement("svg", __assign({ width: '20', height: '20', viewBox: '0 0 20 20', fill: 'none', xmlns: 'http://www.w3.org/2000/svg' }, props),
+    react_1.default.createElement("path", { d: 'M10.5417 4.16667C10.5417 3.86751 10.2992 3.625 10 3.625C9.70085 3.625 9.45833 3.86751 9.45833 4.16667L9.45833 9.45833H4.16667C3.86751 9.45833 3.625 9.70085 3.625 10C3.625 10.2992 3.86751 10.5417 4.16667 10.5417H9.45833L9.45833 15.8333C9.45833 16.1325 9.70085 16.375 10 16.375C10.2992 16.375 10.5417 16.1325 10.5417 15.8333V10.5417H15.8333C16.1325 10.5417 16.375 10.2992 16.375 10C16.375 9.70085 16.1325 9.45833 15.8333 9.45833H10.5417V4.16667Z', fill: 'currentColor' }))); });
